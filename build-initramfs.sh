f7a3f4352ed5109f007a01385716d2c571e79614
ChenRuiwei <1982833213@qq.com>
2025-03-30 02:35:16 +0800

    chore(build): remove unnesessary build scripts

--- build-initramfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ./initramfs/ || exit
-find . | cpio -ov --format=newc >../initramfs.cpio
-cd .. || exit
--- run-initramfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-qemu-system-x86_64 \
-    -kernel ./arch/x86/boot/bzImage \
-    -initrd ./initramfs.cpio \
-    -nographic \
-    -serial mon:stdio \
-    -append "console=ttyS0"
